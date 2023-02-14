414afc5a92fd74bf437ff22391157424879c3a41
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-02-14 09:19:48 +0100

    does compile, now Test !

--- magic_enum_test/main/main.cpp
+++ magic_enum_test/main/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+
+
+
+extern int make_a_test();
+
+
+
 /* Inside .cpp file, app_main function must be declared with C linkage */
 extern "C" void app_main(void)
 {
+
+	make_a_test();
 
 }
